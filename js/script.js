c083798db5cc6f9a1beadab4cbc2117925ffe7e1
feisalahmed <feizulf@gmail.com>
2019-10-24 15:00:45 +0300

    added var script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var century, year, dayOfMonth, dayOfWeek, day;
+//Get input
+function getInput(){
+    century=parseInt(document.getElementById("century").value);
+    year =parseInt(document.getElementById("year").value);
+    month =parseInt(document.getElementById("month").value);
+    dayOfMonth =parseInt(document.getElementById("monthDay").value);
+
+    if(century == ""){
+        alert("input correct century");
+        return false;
+    }else if(year == ""){
+        alert("input correct year");
+        return false;
+    }else if
+};
