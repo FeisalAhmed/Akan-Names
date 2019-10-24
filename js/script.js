3d2a57a051b52db3f895843f6e8a40e5e50266ea
feisalahmed <feizulf@gmail.com>
2019-10-24 15:45:33 +0300

    added main caller functtion

--- js/script.js
+++ js/script.js
@@ -1,36 +1,41 @@
 var century, year, dayOfMonth, dayOfWeek, day;
 //Get input
-function getInput(){
-    century=parseInt(document.getElementById("century").value);
-    year =parseInt(document.getElementById("year").value);
-    month =parseInt(document.getElementById("month").value);
-    dayOfMonth =parseInt(document.getElementById("monthDay").value);
+function getInput() {
+    century = parseInt(document.getElementById("century").value);
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    dayOfMonth = parseInt(document.getElementById("monthDay").value);
 
-    if(century == ""){
+    if (century == "") {
         alert("input correct century");
         return false;
-    }else if(year == ""){
+    } else if (year == "") {
         alert("input correct year");
         return false;
-    }else if(month == ""){
+    } else if (month == "") {
         alert("input correct month");
         return false;
-    }else if(dayOfMonth == ""){
+    } else if (dayOfMonth == "") {
         alert("input correct dayOfMonth");
         return false;
     };
 };
 //Calculate func
-function calculateDay(){
+function calculateDay() {
     getInput();
-    dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + dayOfMonth) % 7) -1;
+    dayOfWeek = ((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + dayOfMonth) % 7) - 1;
     console.log(dayOfWeek); //Test the calculateDay function
     return (Math.floor(dayOfWeek));
     if (dayOfWeek < 0) {
-      dayOfWeek = dayOfWeek * -1;
+        dayOfWeek = dayOfWeek * -1;
     }
     else if (dayOfWeek > 0) {
-      return dayOfWeek;
+        return dayOfWeek;
     }
- };
- 
+};
+//main caller func
+function checkDayOfWeek() {
+    day = calculateDay();
+    checkGender();
+    console.log("The function runs");//Test chackDayOfWeek function
+};
